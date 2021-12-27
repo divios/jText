dedbd068a9fc1efc5ed5dbbbde20f321d37f172c
sergiobernalparrondo <serberpar@gmail.com>
2021-12-27 18:36:50 +0100

    improved api

---
+++ src/main/java/io/github/divios/jtext/parsers/legacyColorsParser.java
@@ -0,0 +1,14 @@
+package io.github.divios.jtext.parsers;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class legacyColorsParser {
+
+    public String parse(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    public String unparse(String s) {
+        return ChatColor.stripColor(s);
+    }
+}
