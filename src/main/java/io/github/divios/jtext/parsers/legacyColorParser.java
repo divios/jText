53a26188d43c8946bca0fdd074608e31c8533a89
sergiobernalparrondo <serberpar@gmail.com>
2022-01-20 11:15:04 +0100

    Added x to unParse

--- src/main/java/io/github/divios/jtext/parsers/legacyColorParser.java
+++ src/main/java/io/github/divios/jtext/parsers/legacyColorParser.java
@@ -11,7 +11,7 @@ public class legacyColorParser {
     public String unparse(String s) {
         char[] array = s.toCharArray();
         for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] == ChatColor.COLOR_CHAR && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(array[i + 1]) != -1) {
+            if (array[i] == ChatColor.COLOR_CHAR && "x0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(array[i + 1]) != -1) {
                 array[i] = '&';
                 array[i + 1] = Character.toLowerCase(array[i + 1]);
             }
